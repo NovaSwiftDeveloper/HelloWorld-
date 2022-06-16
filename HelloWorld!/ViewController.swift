59f4f6d9b45ea112de07ea8c20f39a77be38b3e3
Ravil Khusnullin <studio.edition.rh@icloud.com>
2022-06-16 18:55:36 +0300

    Final easy construction apps

--- HelloWorld!/ViewController.swift
+++ HelloWorld!/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var helloWorldLabel: UILabel!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        startButton.layer.cornerRadius = 10
+        helloWorldLabel.textColor = .systemRed
     }
 
-
+    @IBAction func startPressed() {
+        helloWorldLabel.isHidden.toggle()
+        
+        if helloWorldLabel.isHidden {
+            startButton.setTitle("Show Text", for: .normal)
+        } else {
+            startButton.setTitle("Hide Text", for: .normal)
+        }
+    }
+    
 }
 
+ 
